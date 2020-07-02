8152c00deb7aaf2d8b64ac85a1a9a9955ee31f60
olek <oleklorenc2000@gmail.com>
2020-07-02 20:43:46 +0200

    make it better

--- public/js/game.js
+++ public/js/game.js
@@ -11,9 +11,9 @@ var config = {
   physics: {
     default: "arcade",
     arcade: {
-			debug: true,
-			gravity: { y: 0 }
-		}
+      debug: true,
+      gravity: { y: 0 },
+    },
   },
   scene: {
     preload: preload,
@@ -26,12 +26,14 @@ var game = new Phaser.Game(config);
 
 var stars = [];
 var movementSpeed = 500;
-var camera
-var flag=true
-var lastShot=0;
-var canAddCollider=true
-var canShoot=true
-var bulletsShot=0
+var camera;
+var flag = true;
+var lastShot = 0;
+var canAddCollider = true;
+var canShoot = true;
+var bulletsShot = 0;
+var bulletSpeed = 700;
+var inGame = false;
 
 function preload() {
   this.load.image("star", "assets/star.png");
@@ -40,9 +42,9 @@ function preload() {
 }
 
 function create() {
-  this.socket = io();
+  this.socket = io(); //Inicjalizujemy socketa
 
-  camera=this.cameras.main//.startFollow(this.ship, true, 0.1, 0.1);
+  camera = this.cameras.main; //.startFollow(this.ship, true, 0.1, 0.1);
 
   //Add stars
   for (let i = 0; i < 100; i++) {
@@ -58,272 +60,315 @@ function create() {
   this.physics.world.setBounds(0, 0, config.width, config.height);
   //Cursors
   //this.cursors = this.input.keyboard.createCursorKeys();
-  this.cursors=this.input.keyboard.addKeys(
-    {up:Phaser.Input.Keyboard.KeyCodes.W,
-    down:Phaser.Input.Keyboard.KeyCodes.S,
-    left:Phaser.Input.Keyboard.KeyCodes.A,
-    right:Phaser.Input.Keyboard.KeyCodes.D,
-    space: Phaser.Input.Keyboard.KeyCodes.SPACE});
+  this.cursors = this.input.keyboard.addKeys({
+    up: Phaser.Input.Keyboard.KeyCodes.W,
+    down: Phaser.Input.Keyboard.KeyCodes.S,
+    left: Phaser.Input.Keyboard.KeyCodes.A,
+    right: Phaser.Input.Keyboard.KeyCodes.D,
+    space: Phaser.Input.Keyboard.KeyCodes.SPACE,
+  });
 
   this.cameras.main.setZoom(1);
   this.cameras.main.setBackgroundColor("#246810");
 
-
   //STRZELANIE
-  this.input.on('pointerdown', function (pointer) {
+  this.input.on(
+    "pointerdown",
+    function (pointer) {
+      if (canShoot && this.ship) {
+        shootBullet(this);
+        bulletsShot++;
+        canShoot = false;
+        setTimeout(() => {
+          canShoot = true;
+        }, 1000);
+      }
+      //this.add.image(pointer.x, pointer.y, 'logo');
+    },
+    this
+  );
 
-    if(canShoot && this.ship){
-      shootBullet(this)
-      bulletsShot++
-      canShoot=false;
-      setTimeout(()=>{canShoot=true}, 1000)
-    }
-    //this.add.image(pointer.x, pointer.y, 'logo');
-
-  }, this);
-
-  //Bullets group
-  this.bullets=this.physics.add.group()
-  
-
-  //Socket io
-  var self = this;
+  //Bullets group and otherPlayersGroup
+  this.bullets = this.physics.add.group();
   this.otherPlayers = this.physics.add.group();
 
-  this.socket.on("currentPlayers", function (players) {
-    Object.keys(players).forEach(function (id) {
-      if (players[id].playerId === self.socket.id) {
-        addPlayer(self, players[id]);
-      } else {
-        addOtherPlayers(self, players[id]);
-      }
+  // Name form and proper entry point to the game
+  document.getElementById("button").addEventListener("click", () => {
+    document.querySelector(".bg-modal").style.display = "none";
+    // Ustawiamy flage inGame na true - WAZNE
+    inGame = true;
+    //ULTRA Ważne żeby wyczyścić planszę z dotychczaswoych graczy- inaczej po ponownym zaczęciu gry pojawią się 'sobowtóry'- 2 tekstury na sobie
+    this.otherPlayers.getChildren().forEach((otherPlayer) => {
+      otherPlayer.destroy();
+    });
+    //console.log(document.getElementById('nameInput').value)
+    console.log(this.socket.id);
+    this.socket.emit("newPlayerConfirmed", {
+      id: this.socket.id,
+      name: document.getElementById("nameInput").value,
     });
   });
+
+  //Socket io
+  var self = this; //zeby przesylac zmienną self do funkcji
+
+  //Collider bulletsGroup, otherPlayers(Group)
+  this.physics.add.collider(
+    this.otherPlayers,
+    this.bullets,
+    (otherPlayer, bullet) => {
+      console.log("Hit");
+      bullet.destroy();
+      otherPlayer.getAt(0).destroy(); //otherPlayer to kontener zawierający sprite i napis- stąd otherPlayer.getAt(0)
+      //otherPlayer.getAt(1).destroy()
+      otherPlayer.destroy(); //chyba na wszelki wypadek
+
+      /*
+    self.socket.emit("playerDestroyed", {
+      id: self.socket.id,
+      bulletTag: bullet.type,
+    });*/
+    }
+  );
+
+  //Dodaj starych graczy u nowego gracza
+  this.socket.on("currentPlayers", function (players) {
+    if (inGame) {
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === self.socket.id) {
+          addPlayer(self, players[id]);
+        } else {
+          addOtherPlayers(self, players[id]);
+        }
+      });
+    }
+  });
+
+  //Dodaj nowego gracza u starych graczy
   this.socket.on("newPlayer", function (playerInfo) {
-    addOtherPlayers(self, playerInfo);
+    if (inGame) addOtherPlayers(self, playerInfo);
   });
 
   this.socket.on("playerMoved", function (playerInfo) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-      if (playerInfo.playerId === otherPlayer.playerId) {
+      if (playerInfo.playerId === otherPlayer.getAt(0).playerId) {
+        //TUTAJ
         //otherPlayer.setRotation(playerInfo.rotation);
+        console.log("jestem tutaj");
         otherPlayer.setPosition(playerInfo.x, playerInfo.y);
       }
     });
   });
 
   this.socket.on("bulletShot", function (bulletData) {
-    /*
-    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-      if (playerInfo.playerId === otherPlayer.playerId) {
-        //otherPlayer.setRotation(playerInfo.rotation);
-        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-      }
-    });*/
     // Add bullet sprite
-    self.bullet=self.physics.add.sprite(bulletData.x,bulletData.y,"bullet")
-    self.bullet.type=bulletData.tag
-    // Add bullet to the bullets group
-    self.bullets.add(self.bullet)
-    // Set bullets velocity
-    self.bullet.setVelocityX(bulletData.velocityX)
-    self.bullet.setVelocityY(bulletData.velocityY)
-    /*
-    self.physics.add.collider(
-      self.ship,
-      self.bullet,
-      (invader, laser) => {
-        console.log("Hit")
-        //Emit that this player has been shot with a specific bullet
-        self.bullet.destroy()
-        self.socket.emit("playerDestroyed", {
-          id: self.socket.id,
-        });
-      }
-    );*/
-    console.log("Received bullet data shot by someone")
+    if (inGame) {
+      // Wazne zeby sprawdzic czy jestesmy w grze
+      self.bullet = self.physics.add.sprite(
+        bulletData.x,
+        bulletData.y,
+        "bullet"
+      );
+      self.bullet.type = bulletData.tag;
+      self.bullet.setScale(0.02);
+      // Add bullet to the bullets group
+      self.bullets.add(self.bullet); // tutaj ustawia nam się collider
+      // Set bullets velocity
+      self.bullet.setVelocityX(bulletData.velocityX);
+      self.bullet.setVelocityY(bulletData.velocityY);
+
+      console.log("Received bullet data shot by someone");
+    }
   });
 
-  this.socket.on("playerShotWithBullet", (data)=> {
+  /*
+  this.socket.on("playerShotWithBullet", (data) => {
     // Update all other players about someones demise
-    console.log("Someone was killed but not me!")
+    console.log("Someone was killed but not me!");
     //Problem z data.tag- dostajemy undefined
-    console.log("Bullets type: "+data.tag)
+    console.log("Bullets type: " + data.tag);
     // Iterate through bulltsGroup and delete the one which killed someone, delete killed player avatar
-    var activeBullets=this.bullets.getChildren()
-    activeBullets.forEach((bullet)=>{
-      if(bullet.type==data.bulletTag){
-        bullet.destroy()
+    var activeBullets = this.bullets.getChildren();
+    activeBullets.forEach((bullet) => {
+      if (bullet.type == data.bulletTag) {
+        bullet.destroy();
       }
-    })
+    });
 
-    this.otherPlayers.getChildren().forEach((player)=>{
-      if(player.type==data.id){
-        player.destroy()
+    this.otherPlayers.getChildren().forEach((player) => {
+      if (player.type === data.id) {
+        console.log(player.type);
+        console.log(data.id);
+        player.destroy();
+        player = null;
       }
-    })
-
-
-
+    });
   });
+  */
 
   this.socket.on("disconnect", function (playerId) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-      if (playerId === otherPlayer.playerId) {
+      if (playerId === otherPlayer.getAt(0).playerId) {
+        //znowu otherPlayer to kontener a playerId jest zapisane w obiekcie sprite
         otherPlayer.destroy();
       }
     });
   });
-
-  
 }
 
 function update(time, delta) {
   if (this.ship) {
-
-    if(flag){
-      this.cameras.main.startFollow(this.ship, true, 0.1, 0.1);
-
-      this.physics.add.collider(
-        this.ship,
-        this.bullets,
-        (ship, bullet) => {
-          console.log("Hit")
-          //Emit that this player has been shot with a specific bullet
-          
-          // Destroy this player on his client side and emit event to other players
-          bullet.destroy()
-          
-          //Wazne zeby przypisac null, samo destroy() tego nie robi, a jest to potrzebne żeby nie wejść w listenery wejscia wasd w petli update
-          this.ship.destroy()
-          this.ship=null
-
-          console.log(bullet.type)
-          this.socket.emit("playerDestroyed", {
-            id: this.socket.id,
-            bulletTag: bullet.type
-          });
-        }
-      );
-      
-      flag=false
-    }
-
-    if(this.ship){
-
+    if (this.ship) {
       if (this.cursors.left.isDown) {
-        this.ship.setVelocityX(-movementSpeed);
+        this.container.body.setVelocityX(-movementSpeed);
       } else if (this.cursors.right.isDown) {
-        this.ship.setVelocity(movementSpeed);
+        this.container.body.setVelocity(movementSpeed);
       } else {
-        this.ship.setVelocity(0);
+        this.container.body.setVelocity(0);
       }
 
       if (this.cursors.down.isDown) {
-        this.ship.setVelocityY(movementSpeed);
+        this.container.body.setVelocityY(movementSpeed);
       } else if (this.cursors.up.isDown) {
-        this.ship.setVelocityY(-movementSpeed);
+        this.container.body.setVelocityY(-movementSpeed);
       } else {
-        this.ship.setVelocityY(0);
+        this.container.body.setVelocityY(0);
       }
 
-
       // emit player movement
-      var x = this.ship.x;
-      var y = this.ship.y;
-      var r = this.ship.rotation;
+      var x = this.container.x;
+      var y = this.container.y;
+      var r = this.container.rotation;
       if (
-        this.ship.oldPosition &&
-        (x !== this.ship.oldPosition.x ||
-          y !== this.ship.oldPosition.y ||
-          r !== this.ship.oldPosition.rotation)
+        this.container.oldPosition &&
+        (x !== this.container.oldPosition.x ||
+          y !== this.container.oldPosition.y ||
+          r !== this.container.oldPosition.rotation)
       ) {
+        console.log("Detecting movement on client side");
         this.socket.emit("playerMovement", {
-          x: this.ship.x,
-          y: this.ship.y,
-          rotation: this.ship.rotation,
+          x: this.container.x,
+          y: this.container.y,
+          rotation: this.container.rotation,
         });
       }
 
       // save old position data
-      this.ship.oldPosition = {
-        x: this.ship.x,
-        y: this.ship.y,
-        rotation: this.ship.rotation,
+      this.container.oldPosition = {
+        x: this.container.x,
+        y: this.container.y,
+        rotation: this.container.rotation,
       };
     }
-
   }
 }
 
 function addPlayer(self, playerInfo) {
+  self.container = self.add.container(playerInfo.x, playerInfo.y);
   self.ship = self.physics.add
-    .image(playerInfo.x, playerInfo.y, "character")
+    .image(0, 0, "character")
     .setOrigin(0.5, 0.5)
-    .setScale(0.3)
+    .setScale(0.3);
 
-  self.ship.type=playerInfo.playerId
-    //.setDisplaySize(60, 60);
-    //.setCircle(90)
-    
-  //self.ship.body.setOffset(20,20)
-  //.setDisplaySize(53, 40);
-  /*
-  if (playerInfo.team === "blue") {
-    self.ship.setTint(0x0000ff);
-  } else {
-    self.ship.setTint(0xff0000);
-  }*/
+  self.nameText = self.add.text(
+    0,
+    -40,
+    document.getElementById("nameInput").value
+  );
+  self.nameText.setOrigin(0.5, 0.5);
+  self.container.setSize(
+    self.ship.getBounds().width,
+    self.ship.getBounds().height
+  );
+  self.container.add(self.ship);
+  self.container.add(self.nameText);
+  self.physics.world.enable(self.container); //włączamy arcade fizyke -wazne
 
-  /*
-  self.ship.setDrag(100);
-  self.ship.setAngularDrag(100);
-  self.ship.setMaxVelocity(200);
-  */
+  self.container.body.setCollideWorldBounds(true); //włączamy kolizje z worldBounds dla całego kontenera
+
+  self.ship.type = playerInfo.playerId;
+
+  self.cameras.main.startFollow(self.container, true, 0.1, 0.1);
+
+  self.physics.add.collider(self.container, self.bullets, (ship, bullet) => {
+    console.log("Hit");
+
+    bullet.destroy();
+
+    //Wazne zeby przypisac null, samo destroy() tego nie robi, a jest to potrzebne żeby nie wejść w listenery wejscia wasd w petli update
+    self.container.destroy();
+    self.container = null;
+    self.ship = null;
+
+    self.otherPlayers.getChildren().forEach((otherPlayer) => {
+      otherPlayer.destroy();
+    });
+
+    inGame = false;
+
+    document.querySelector(".bg-modal").style.display = "flex";
+
+    console.log(bullet.type);
+
+    self.socket.emit("playerDestroyed", {
+      id: self.socket.id,
+    });
+  });
 }
 
 function addOtherPlayers(self, playerInfo) {
+  console.log("adding some player");
+  const otherPlayerContainer = self.add.container(playerInfo.x, playerInfo.y);
+
   const otherPlayer = self.physics.add
-    .sprite(playerInfo.x, playerInfo.y, "character")
+    .sprite(0, 0, "character")
     .setOrigin(0.5, 0.5)
-    .setScale(0.3)
+    .setScale(0.3);
+  console.log(playerInfo.playerName);
+  const otherPlayerNameText = self.add.text(0, -40, playerInfo.playerName);
 
-  otherPlayer.type=playerInfo.playerId
-    //.setCircle(90)
-    //.setDisplaySize(60, 60);
+  otherPlayerNameText.setOrigin(0.5, 0.5);
+  otherPlayerContainer.setSize(
+    otherPlayer.getBounds().width,
+    otherPlayer.getBounds().height
+  );
+  otherPlayerContainer.add(otherPlayer);
+  otherPlayerContainer.add(otherPlayerNameText);
+  self.physics.world.enable(otherPlayerContainer);
 
-  /*
-  if (playerInfo.team === "blue") {
-    otherPlayer.setTint(0x0000ff);
-  } else {
-    otherPlayer.setTint(0xff0000);
-  }
-  */
+  otherPlayer.type = playerInfo.playerId;
 
   otherPlayer.playerId = playerInfo.playerId;
-  self.otherPlayers.add(otherPlayer);
+  self.otherPlayers.add(otherPlayerContainer);
 }
 
-
 //STRZELANIE
-function shootBullet(self){
-  self.bullet=self.physics.add.sprite(self.ship.x,self.ship.y,"bullet")
+function shootBullet(self) {
+  self.bullet = self.physics.add.sprite(
+    self.container.x,
+    self.container.y,
+    "bullet"
+  );
+  self.bullet.setScale(0.02);
 
-  //var shipPosY= self.ship.y>960 ? self.ship.y%960 : self.ship.y
+  var rad = Phaser.Math.Angle.Between(
+    self.container.x - camera.scrollX,
+    self.container.y - camera.scrollY,
+    game.input.mousePointer.x,
+    game.input.mousePointer.y
+  );
 
-  var velocityX=((game.input.mousePointer.x)-(self.ship.x-camera.scrollX ))
-  var velocityY=((game.input.mousePointer.y)-(self.ship.y-camera.scrollY))
+  velocityX = bulletSpeed * Math.cos(rad);
+  velocityY = bulletSpeed * Math.sin(rad);
 
-  
-
-  self.bullets.add(self.bullet)
-  self.bullet.setVelocity(velocityX, velocityY)
-  self.bullet.type=self.socket.id+bulletsShot
+  self.bullets.add(self.bullet);
+  self.bullet.setVelocity(velocityX, velocityY);
+  self.bullet.type = self.socket.id + bulletsShot;
   //console.log(self.bullet.type)
-  
+
   self.socket.emit("bulletShoting", {
-    x: self.ship.x,
-    y: self.ship.y,
+    x: self.container.x,
+    y: self.container.y,
     velocityX: velocityX,
     velocityY: velocityY,
     tag: self.bullet.type,
@@ -331,5 +376,4 @@ function shootBullet(self){
   });
 
   //OKRESLENIE POZYCJI LEWEGO GORNEGO ROGU EKRANU: camera.scrollX, camera.scrollY
-  
 }
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+var server = require("http").Server(app);
+var io = require("socket.io").listen(server);
 
 var players = {};
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
+io.on("connection", function (socket) {
+  console.log("a user connected");
+
+  /*
   // create a new player and add it to our players object
   players[socket.id] = {
     rotation: 0,
@@ -24,6 +26,24 @@ io.on('connection', function (socket) {
   socket.emit('currentPlayers', players);
   // update all other players of the new player
   socket.broadcast.emit('newPlayer', players[socket.id]);
+  */
+  socket.on("newPlayerConfirmed", (data) => {
+    console.log("someone wats to join the game!");
+
+    // create a new player and add it to our players object
+    players[data.id] = {
+      rotation: 0,
+      x: Math.floor(Math.random() * 700) + 50,
+      y: Math.floor(Math.random() * 500) + 50,
+      playerId: data.id,
+      playerName: data.name,
+    };
+
+    // send the players object to the new player
+    socket.emit("currentPlayers", players);
+    // update all other players of the new player
+    socket.broadcast.emit("newPlayer", players[data.id]);
+  });
 
   // when a player moves, update the player data
   socket.on("playerMovement", function (movementData) {
@@ -35,6 +55,7 @@ io.on('connection', function (socket) {
     socket.broadcast.emit("playerMoved", players[socket.id]);
   });
 
+  
   socket.on("bulletShoting", function (bulletData) {
     //players[socket.id].x = movementData.x;
     //players[socket.id].y = movementData.y;
@@ -44,20 +65,33 @@ io.on('connection', function (socket) {
     socket.broadcast.emit("bulletShot", bulletData);
   });
 
-
-  //Shot with a bullet with specific tag
   socket.on("playerDestroyed", (data)=>{
-    console.log(data)
-    socket.broadcast.emit("playerShotWithBullet", data);
+    console.log(data.id+"has been destroyed")
+    if(players[data.id])
+      delete players[data.id]
   })
 
+
+  /*
+  //Shot with a bullet with specific tag
+  socket.on("playerDestroyed", (data) => {
+    console.log("przed usunieciem:"+players);
+    delete players[data.id];
+    console.log("po usunieciu:" +players)
+    socket.broadcast.emit("playerShotWithBullet", data);
+  });
+  */
+
+  
   // when a player disconnects, remove them from our players object
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+    console.log(players)
     // remove this player from our players object
+    if(players[socket.id])
     delete players[socket.id];
     // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
+    io.emit("disconnect", socket.id);
   });
 });
 
